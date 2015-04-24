ba41f15cb722956ea4489b89a54bf985a0e6a61a
Francois Beaufort <beaufort.francois@gmail.com>
2015-04-24 15:40:49 +0200

    Added password encryption feature

--- src/module.cc
+++ src/module.cc
@@ -32,32 +32,22 @@ class Instance : public pp::Instance {
     void *buffer = archive_buffer.Map();
     size_t used = 0;
 
-    int r1 = archive_write_set_format_zip(a);
-    int r2 = archive_write_add_filter_none(a);
-    int r3 = archive_write_open_memory(a, buffer, kMaxBufferSize, &used);
-    //int r4 = archive_write_zip_set_compression_deflate(a);
-    //int r0 = archive_write_set_options(a, "zip:encryption=aes256");
-    //int r4 = archive_write_set_passphrase(a, password.c_str());
+    archive_write_set_format_zip(a);
+    archive_write_add_filter_none(a);
+    archive_write_set_options(a, "zip:encryption=traditional");
+    archive_write_set_passphrase(a, password.c_str());
+    archive_write_open_memory(a, buffer, kMaxBufferSize, &used);
 
     archive_entry *entry = archive_entry_new();
     archive_entry_copy_pathname(entry, filename.c_str());
     archive_entry_set_mode(entry, AE_IFREG | 0755);
     archive_entry_set_size(entry, data.ByteLength());
-    int r5 = archive_write_header(a, entry);
+    archive_write_header(a, entry);
     archive_entry_free(entry);
 
-    int r6 = archive_write_data(a, data.Map(), data.ByteLength());
+    archive_write_data(a, data.Map(), data.ByteLength());
     archive_write_close(a);
     archive_write_free(a);
-
-    //dict.Set("r0", r0);
-    dict.Set("r1", r1);
-    dict.Set("r2", r2);
-    dict.Set("r3", r3);
-    //dict.Set("r4", r4);
-    dict.Set("r5", r5);
-    dict.Set("r6", r6);
-    dict.Set("used", int(used));
 
     pp::VarArrayBuffer encryptedData(used);
     memcpy(encryptedData.Map(), archive_buffer.Map(), used);
